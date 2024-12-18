ce746461382cf6afce686284227a3d3053605bed
Tayte <t82020@outlook.com>
2024-12-18 11:23:46 -0600

    Just an idea

---
+++ ScrabbleScoreCounter.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void vppl(int people){
+    for(int i = 0; i < people; ++i){
+        
+    }
+}
+
+
+int main(){
+    int people;
+
+    cout << "Hello Welcome to Scrabble Score Counter!" << endl;
+    cout << "How many people are playing today?" << endl;
+    cin >> people;
+    
+
+
+
+
+
+}
